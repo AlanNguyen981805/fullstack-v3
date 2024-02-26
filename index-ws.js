355250deb4cf185d7d7741778970fcab31a6272d
Leo99972 <hiep.nguyen5@ntq-solution.com.vn>
2024-02-26 11:40:42 +0700

    initial web socket

---
+++ index-ws.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const server = require('http').createServer();
+const app = express();
+const PORT = 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: __dirname});
+});
+
+server.on('request', app);
+
+server.listen(PORT, function () { console.log('Listening on ' + PORT); });
+
+// Begin websocket
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ server: server })
+
+wss.on('connection', function connection(ws) {
+    console.log("ws", ws.readyState === ws.OPEN)
+    const numClients = wss.clients.size;
+    console.log('Client connected', numClients);
+
+    wss.broadcast(`current visitor ${numClients}`)
+
+    if(ws.readyState === ws.OPEN) {
+        ws.send('Welcome to my server')
+    }
+
+    ws.on('close', function close() {
+        console.log('A client has disconnected')
+    })
+})
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data)
+    })
+}
